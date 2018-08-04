8b70fe8b71444378895d80148beb6c4054f00e8b
Wais <waisibrahim77@gmail.com>
2018-08-04 15:53:54 +0700

    First add

---
+++ Source/IslandProject/IslandProjectGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "IslandProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ISLANDPROJECT_API AIslandProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
